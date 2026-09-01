7ad64e29e72addec317352f0239e543c4295c1f7
Hady AL Saleem <hadisaleem50@gmail.com>
2026-09-01 16:39:40 +0300

    task 1.1 & 1.2 done

--- dart_lab02/lib/main.dart
+++ dart_lab02/lib/main.dart
@@ -1,14 +1,23 @@
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
 
+                                  //Task 1.1
+// void main() {
+//   int age = 25;
+//   double height = 1.65;
+//   bool isStudent = false;
+//   String name = 'Hady AL Saleem';
+//   List<int> scores = [90, 80, 70];
+
+//   print('$name is $age years old and $height meters tall.');
+//   print('Student: $isStudent');
+//   print('Scores: $scores');
+// }
+
+                                  //Task 1.2
 void main() {
-  int age = 25;
-  double height = 1.80;
-  bool isStudent = false;
-  String name = 'Ahmad';
-  List<int> scores = [90, 80, 70];
+var city = 'Qatif';
+print('City: $city');
 
-  print('$name is $age years old and $height meters tall');
-  print('Student: $isStudent');
-  print('Scores: $scores');
+  city = 'Khobar';
+  print('after change \nCity: $city');
 }
-
