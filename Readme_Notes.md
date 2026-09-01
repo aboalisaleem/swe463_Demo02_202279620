6af51d0b4f1ccfe69bd96bb57b7994f409ad3a17
Hady AL Saleem <hadisaleem50@gmail.com>
2026-09-01 17:12:28 +0300

    task 2.1 done

---
+++ Readme_Notes.md
@@ -0,0 +1,9 @@
+#Task 2.1
+**What is the difference between final and const?**
+    -final variables can only be set once and are initialized when accessed, while const variables are compile-time constants and must be initialized at the time of declaration.
+    **Why can dynamic change from String to int?**
+    -Because it dose not have a fixed type.
+
+#Task 3.1
+
+
--- dart_lab02/lib/main.dart
+++ dart_lab02/lib/main.dart
@@ -14,10 +14,32 @@
 // }
 
                                   //Task 1.2
-void main() {
-var city = 'Qatif';
-print('City: $city');
+// void main() {
+// var city = 'Qatif';
+// print('City: $city');
 
-  city = 'Khobar';
-  print('after change \nCity: $city');
+//   city = 'Khobar';
+//   print('after change \nCity: $city');
+// }
+
+
+
+                                  //Task 2.1
+    // What is the difference between final and const?
+    // final variables can only be set once and are initialized when accessed, while const variables are compile-time constants and must be initialized at the time of declaration.
+    // Why can dynamic change from String to int?
+    // Because it dose not have a fixed type.
+void main() {
+  final String courseName = 'Mobile App Development';
+  const double pi = 3.14159;
+
+  dynamic anything = 'I can be any type!';
+  //courseName = 'Web Development'; // as final cannot be changed
+  //pi = 3.14; // as const cannot be changed
+  print(courseName);
+  print(pi);
+  print(anything);
+
+  anything = 42;
+  print(anything);
 }
