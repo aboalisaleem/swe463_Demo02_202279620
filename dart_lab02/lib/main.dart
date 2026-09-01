37f64a150407c3a120811148d8049997ac3b313a
Hady AL Saleem <hadisaleem50@gmail.com>
2026-09-01 10:49:13 +0300

    task 1.1 code

--- dart_lab02/lib/main.dart
+++ dart_lab02/lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-}
+  int age = 25;
+  double height = 1.80;
+  bool isStudent = false;
+  String name = 'Ahmad';
+  List<int> scores = [90, 80, 70];
 
+  print('$name is $age years old and $height meters tall');
+  print('Student: $isStudent');
+  print('Scores: $scores');
+}
 
