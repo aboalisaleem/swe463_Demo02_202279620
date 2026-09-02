1b36a81acc59410c3d7c1b76863fcbfef84f99e3
Hady AL Saleem <hadisaleem50@gmail.com>
2026-09-02 21:27:16 +0300

    task 8.1 done

--- dart_lab02/lib/main.dart
+++ dart_lab02/lib/main.dart
@@ -324,7 +324,28 @@
 
                               //Task 8.1
 
+// Future<String> fetchData() async {
+//   await Future.delayed(const Duration(seconds: 2));
+//   return 'Fetched Data';
+// }
 
+// Future<void> main() async {
+//   print('[1] Using async/await');
+//   print('[2] Fetching data...');
+
+//   final data = await fetchData();
+//   print('[3] Result: $data');
+
+//   print('[4] Using .then()');
+
+//   fetchData().then((value) {
+//     print('[6] .then() result: $value');
+//   }).catchError((error) {
+//     print('[7] Error: $error');
+//   });
+
+//   print('[5] Program continues after .then() call');
+// }
 
 
 
